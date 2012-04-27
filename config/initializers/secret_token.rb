8d605d147eb8621d26fe798ec62d6cd558832352
HeadFlow <headflow@HeadFlowui-iMac.local>
2012-04-27 13:37:53 +0900

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SampleApp::Application.config.secret_token = '286a8a83010047a2dd34cd43e5dffc979f1e8c9a6b76b570ffd9ca6bb62b4a8a0a87bf840cd5b0f12033a467d800380cb420e228012be84f142078c05571f8df'
